eebab4406586b24581cd6691b9a03d89e9a74287
Jon Surrell <sirreal@users.noreply.github.com>
2024-01-05 15:04:05 +0100

    Add true shorthand for requestToExternalModule

--- packages/dependency-extraction-webpack-plugin/lib/util.js
+++ packages/dependency-extraction-webpack-plugin/lib/util.js
@@ -67,7 +67,7 @@ function defaultRequestToExternal( request ) {
  */
 function defaultRequestToExternalModule( request ) {
 	if ( request === '@wordpress/interactivity' ) {
-		return request;
+		return true;
 	}
 }
 
